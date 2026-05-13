d0d815433e04a7fbca4e75abc0d5263b71dbadbf
Fábio <fabiocassiano@jfes.jus.br>
2026-05-13 13:21:19 -0300

    fix: move painel de administração de monster config

--- apps/engine/killstats/admin.py
+++ apps/engine/killstats/admin.py
@@ -1,1 +1,19 @@
 # Register your models here.
+from typing import TYPE_CHECKING
+
+from django.contrib import admin
+
+from .models.monster_config import MonsterConfig as MonsterConfig
+
+if TYPE_CHECKING:
+    # Isso só existe durante a análise do Mypy
+    BaseAdmin = admin.ModelAdmin[MonsterConfig]
+else:
+    # Isso é o que o Django usa em tempo de execução
+    BaseAdmin = admin.ModelAdmin
+
+
+@admin.register(MonsterConfig)
+class MonsterConfigAdmin(BaseAdmin):
+    list_display = ("monster", "min_interval", "max_interval")
+    search_fields = ("monster__name",)
